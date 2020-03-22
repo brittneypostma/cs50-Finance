1ece34c23fc55d4d65b872094ca08ffb11ac6d44
Brittney <brittney.postma@hotmail.com>
2020-03-22 18:08:57 -0400

    print statement

--- app.py
+++ app.py
@@ -191,6 +191,8 @@ def login():
         rows = db.execute("SELECT * FROM users WHERE username = ?",
                          (request.form.get("username")))
 
+        print(request.form.get("username"))
+
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
